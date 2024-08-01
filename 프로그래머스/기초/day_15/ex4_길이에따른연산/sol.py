3d9e8a5b3d6b85c3065aaafc23ebac49cd1f5c8a
sabeom <sabumi2002@naver.com>
2024-08-02 08:48:21 +0900

    24.08.02 day_15(ex4)

---
+++ 프로그래머스/기초/day_15/ex4_길이에따른연산/sol.py
@@ -0,0 +1,20 @@
+# my solution
+from functools import reduce
+def solution(num_list):
+    def func(x, y):
+        return x*y
+    answer = sum(num_list) if len(num_list) >= 11 else reduce(func, num_list)
+    return answer
+
+# best solution (1)
+def solution(num_list):
+    if len(num_list) >= 11:
+        return eval('+'.join(list(map(str, num_list))))
+    else:
+        return eval('*'.join(list(map(str, num_list))))
+    
+# best solution (2)
+from math import prod
+def solution(num_list):
+    return sum(num_list) if len(num_list)>=11 else prod(num_list)
+
