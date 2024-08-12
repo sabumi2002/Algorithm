16b700ef54ddc307e86f57f3fc386bb325e981f3
sabeom <sabumi2002@naver.com>
2024-08-13 08:30:49 +0900

    24.08.13 공백으로구분하기1

---
+++ 프로그래머스/기초/day_17/ex4_공백으로구분하기1/sol.py
@@ -0,0 +1,11 @@
+# my solution
+def solution(my_string):
+    answer = list(map(str, my_string.split(' ')))
+    return answer
+
+
+# best solution
+def solution(my_string):
+    return my_string.split(' ')
+
+
