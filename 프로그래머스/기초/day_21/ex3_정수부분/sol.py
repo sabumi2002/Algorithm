6bb1c6cadc84a9f378c7c141b662437860a2f62b
sabeom <sabumi2002@naver.com>
2024-09-02 07:47:02 +0900

    24.09.02 정수부분

---
+++ 프로그래머스/기초/day_21/ex3_정수부분/sol.py
@@ -0,0 +1,13 @@
+# my solution
+def solution(flo):
+    answer = int(flo)
+    return answer
+
+# best solution(1)
+def solution(flo):
+    return flo//1
+
+# best solution(2)
+from math import floor
+def solution(flo):
+    return floor(flo)
