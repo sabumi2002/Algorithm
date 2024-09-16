95d65f5cb5f297844ac4c77b52793eb8a06f7638
sabeom <sabumi2002@naver.com>
2024-09-16 16:08:08 +0900

    24.09.16 날짜비교하기

--- 프로그래머스/기초/day_23/ex5_날짜비교하기/sol.py
+++ 프로그래머스/기초/day_23/ex5_날짜비교하기/sol.py
@@ -14,3 +14,9 @@ def solution(date1, date2):
 def solution(date1, date2):
     return int(date1 < date2)
 
+# best solution (2)
+def solution(date1, date2):
+    for i in range(3):
+        if date1[i]<date2[i]:return 1
+        elif date2[i]<date1[i]: return 0
+    return 0
