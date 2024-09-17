e37dd12f431eef296a78c302e52a05cd18b1858b
sabeom <sabumi2002@naver.com>
2024-09-17 11:00:11 +0900

    24.09.17 커피심부름

---
+++ 프로그래머스/기초/day_24/ex1_커피심부름/sol.py
@@ -0,0 +1,25 @@
+# my solution
+def solution(order):
+    def orderProc(order):
+        orderSet = {"americano":4500, "latte":5000}
+        result = 4500
+        for x, y in orderSet.items():
+            if x in order:
+                result = y
+                break
+        return result
+    answer = 0
+    for x in order:
+        answer += orderProc(x)
+    return answer
+
+# best solution
+def solution(order):
+    answer = 0
+    for want in order:
+        if 'latte' in want:
+            answer += 500
+        answer += 4500
+
+    return answer
+
