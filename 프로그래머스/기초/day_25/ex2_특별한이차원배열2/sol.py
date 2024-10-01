418a8e0469f7c67a005f904c568722907a5269f2
sabeom <sabumi2002@naver.com>
2024-10-01 22:35:09 +0900

    24.10.01 특별한이차원배열2

--- 프로그래머스/기초/day_25/ex2_특별한이차원배열2/sol.py
+++ 프로그래머스/기초/day_25/ex2_특별한이차원배열2/sol.py
@@ -1,6 +1,12 @@
 # my solution
-
-
+def solution(arr):
+    answer = 1
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if arr[i][j] != arr[j][i]:
+                answer = 0
+    return answer
 
 # best solution
-
+def solution(arr):
+    return int(arr == list(map(list, zip(*arr))))
