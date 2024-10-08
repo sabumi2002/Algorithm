30bdb3b570f8a7eebe63db2edde5746355829173
sabeom <57219910+sabumi2002@users.noreply.github.com>
2024-10-08 16:23:37 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 308 ms, Memory: 31180 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
@@ -1,15 +1,15 @@
 import sys
-n = int(sys.stdin.readline().rstrip())
-for _ in range(n):
-    s = input()
-    count = 0
-    def recursion(s, l, r, count):
+def recursion(s, l, r, count):
         count += 1
         if(l >= r): return 1, count
         elif (s[l] != s[r]): return 0, count
         else: return recursion(s, l+1, r-1, count)
-    def isParlindrome(s):
-        count = 0
-        return recursion(s, 0, len(s)-1, count)
+def isParlindrome(s):
+    count = 0
+    return recursion(s, 0, len(s)-1, count)
+
+n = int(sys.stdin.readline().rstrip())
+for _ in range(n):
+    s = sys.stdin.readline().rstrip()
     answer, count = isParlindrome(s)
     print(f"{answer} {count}")
