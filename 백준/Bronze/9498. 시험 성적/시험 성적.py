06c22f1db3001856e91f513cfadddc51de0065a1
sabeom <57219910+sabumi2002@users.noreply.github.com>
2024-11-06 21:46:12 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+import sys
+n = int(sys.stdin.readline().strip())
+if (90<=n<=100):
+    print("A")
+elif (80<=n):
+    print("B")
+elif (70<=n):
+    print("C")
+elif (60<=n):
+    print("D")
+else:
+    print("F")
